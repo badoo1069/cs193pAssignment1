2d7ff3cad187fce39d28adfc2e9ca00899eac556
saurabh <saurabhmail123@gmail.com>
2015-03-06 20:52:33 +0530

    fix the working of pi

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -68,7 +68,7 @@ class ViewController: UIViewController {
     func performOperation(operation: (Double, Double) -> Double) {
         if operandStack.count >= 2 {
             displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
-            enter()
+            enter()            
         }
     }
     
